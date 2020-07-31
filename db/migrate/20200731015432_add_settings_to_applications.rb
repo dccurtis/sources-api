95a7218c5af3bf55ae80ac96fc0683ef5aa92072
Douglas Curtis <docurtis@redhat.com>
2020-07-30 21:54:44 -0400

    changing migration file

--- db/migrate/20200731015432_add_settings_to_applications.rb
+++ db/migrate/20200731015432_add_settings_to_applications.rb
@@ -1,4 +1,4 @@
-class AddOpaqueToApplications < ActiveRecord::Migration[5.2]
+class AddSettingsToApplications < ActiveRecord::Migration[5.2]
   def change
     add_column :applications, :settings, :jsonb
   end
